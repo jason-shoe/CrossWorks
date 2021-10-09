0cf145452f1ef740c09a785168e503ec839637b1
Jason Xu <gwangsenshoe@gmail.com>
2021-10-09 16:48:37 -0400

    backend boilerplate

---
+++ backend/src/main/java/com/javabackend/crossworks/CrossworksApplication.java
@@ -0,0 +1,13 @@
+package com.javabackend.crossworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossworksApplication.class, args);
+	}
+
+}
