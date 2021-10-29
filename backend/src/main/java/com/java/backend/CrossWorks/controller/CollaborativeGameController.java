8f765e7a0cc4ae157c4a4047048265295a7e76fb
Jason Xu <gwangsenshoe@gmail.com>
2021-10-29 13:32:36 -0400

    adding backend endpoint for dates

--- backend/src/main/java/com/java/backend/CrossWorks/controller/CollaborativeGameController.java
+++ backend/src/main/java/com/java/backend/CrossWorks/controller/CollaborativeGameController.java
@@ -3,18 +3,23 @@ package com.java.backend.CrossWorks.controller;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
 import com.java.backend.CrossWorks.collaborative.CollaborativeGame;
 import com.java.backend.CrossWorks.collaborative.Player;
 import com.java.backend.CrossWorks.models.Crossword;
 import com.java.backend.CrossWorks.service.CrosswordService;
 import com.java.backend.CrossWorks.service.CollaborativeGameService;
+import com.java.backend.CrossWorks.service.Views;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.Vector;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/collaborative-game")
 public class CollaborativeGameController {
     @Autowired
@@ -30,6 +35,13 @@ public class CollaborativeGameController {
     @JsonIgnoreProperties("answers")
     public Crossword sampleCrossword() {
         return crosswordService.getFirst();
+    }
+
+    @GetMapping("/dates")
+    @ResponseBody
+    @JsonView(Views.CrosswordIdDateView.class)
+    public Vector<Crossword> getAllDates() {
+        return crosswordService.getDates();
     }
 
     @PostMapping("/start")
